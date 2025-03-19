c4dfc8273aad6ae4fb3b5761419de563d995aebd
Lucas Montorio <lucas.montorio@lifen.fr>
2025-03-19 13:50:58 +0000

    Chore: Make gem autoloadable with a Railtie

---
+++ lib/rspec_time_guard/railtie.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module RspecTimeGuard
+  class Railtie < Rails::Railtie
+    initializer 'rspec_time_guard.configure' do
+      Rails.logger.info('INITIALIZER')
+
+      config.after_initialize do
+        Rails.logger.info('AFTER INITIALIZE')
+
+        if defined?(RSpec) && Rails.env.test?
+          require 'rspec_time_guard'
+
+          RspecTimeGuard.setup if defined?(RSpec)
+        end
+      end
+    end
+  end
+end
